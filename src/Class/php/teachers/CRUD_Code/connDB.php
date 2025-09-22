5aeb542e54063e6dba0c627caf960d59c3ac0ba0
sobreeH <wansobree.hajihama@gmail.com>
2025-09-22 15:39:03 +0700

    psuedo xampp complete

--- src/Class/php/teachers/CRUD_Code/connDB.php
+++ src/Class/php/teachers/CRUD_Code/connDB.php
@@ -1,11 +1,15 @@
 <?php
-$servername = "localhost";
+$servername = "mariadb"; // service name in docker-compose.yaml
 $username = "root";
-$password = "";
+$password = "root"; // must match MYSQL_ROOT_PASSWORD in docker-compose.yaml
+$database = "devdb"; // optional, if you defined MYSQL_DATABASE
 
 // Create connection
 $conn = new mysqli($servername, $username, $password);
+
 // Check connection
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
+echo "Connected successfully!";
+?>
